b319c30156170313a4a984509712b75b845c89e4
Oscar Odic <odic.oscar@gmail.com>
2017-08-11 17:17:25 +0200

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+git stash --all
 sbt "set test in assembly := {}" clean assembly
 export SSHPASS=$2
 sshpass -e scp target/scala-2.11/sijali.jar $3@$1:$4
-sshpass -e ssh $3@$1 $4/deploy.sh
+sshpass -e ssh $3@$1 $4/deploy.sh &
